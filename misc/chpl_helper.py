90b6a5c43ef5af50ac23610e4be7ac128642f5ab
ray <ray.voelker@gmail.com>
2022-01-10 11:10:42 -0500

    added to the helper functions

--- misc/chpl_helper.py
+++ misc/chpl_helper.py
@@ -1,15 +1,10 @@
 # import the needed modules
 from base64 import b64encode
-from io import BytesIO
 import json
-import numpy as np
-import pandas as pd
-import re
 import requests
-from sqlalchemy import create_engine
 
 # this will get an initial access token
-def set_access_headers(client_key, client_secret, base_url):
+def get_access_headers(client_key, client_secret, base_url):
   """
   use this function to set and refresh the access_headers for future
   authorizing API requests
@@ -19,6 +14,91 @@ def set_access_headers(client_key, client_secret, base_url):
     (client_key + ':' + client_secret).encode('utf-8')
   ).decode('utf-8')
 
+  headers = {}
+  headers['authorization'] = 'basic ' + auth_string
+
+  try:
+      r = requests.post(base_url + 'token', headers=headers, verify=True)
+
+  except requests.ConnectionError as e:
+      # TODO: return something more meaningful ...
+      # print('connection error: {}'.format(e))
+      return(
+        -1
+      )
+      return 0
+
+  if r.status_code != 200:
+      print(r.status_code)
+      return(
+        -1
+      )
+
+  access_token = r.json()['access_token']
+
+  # set our headers to use the access token
+  headers['authorization'] = 'bearer ' + access_token
+
+  # Note: depending on the Sierra REST API request endpoint,
+  # you may need to change the types below to fit the request,
+  # but these are pretty standard
+  headers['content-type'] = 'application/json'
+  headers['accept'] = 'application/json'
+
+  return headers
+
+
+def get_api_access_header_info(access_headers, base_url):
+  """
+  try to authorize the client_key + client_secret and return some data
+  """
+
+  try:
+    r = requests.get(base_url + 'info/token', headers=access_headers, verify=True)
+  except:
+    return(
+      # TODO 
+      # {'error': r.NameError}
+      -1
+    )
+
+  return(r.json())
+
+
+def make_auth_string(client_key, client_secret):
+  """
+  given client_key, client_secret inputs, produce the auth_string for the sierra rest api
+  (or, better yet, store this auth string, and use it as the requested input from the user)
+  """
+  auth_string = b64encode(
+      (client_key + ':' + client_secret).encode('utf-8')
+  ).decode('utf-8')
+
+  return(auth_string)
+
+
+def make_auth_headers(auth_string):
+  headers = {}
+  headers['authorization'] = 'basic ' + auth_string
+
+  return(headers)
+
+
+def set_access_headers(base_url, auth_string=None, client_key=None, client_secret=None):
+  """
+  use this function to set and refresh the access_headers for future
+  authorizing API requests
+
+  either use the client_key and client_secret 
+  ... or the auth_string (which has been pre-encoded from the key + secret)
+  """
+  if (auth_string is None and client_key is None and client_key is None):
+    # can't work with no input
+    return(-1)
+
+  if ( (auth_string is None) and (client_key is not None and client_key is not None) ):
+    auth_string = make_auth_string(client_key, client_secret)
+  
   headers = {}
   headers['authorization'] = 'basic ' + auth_string
 
@@ -48,23 +128,3 @@ def set_access_headers(client_key, client_secret, base_url):
   headers['accept'] = 'application/json'
 
   return headers
-
-
-def check_credentials(client_key, client_secret, base_url):
-  """
-  try to authorize the client_key + client_secret and return some data
-  """
-
-  try:
-    headers = set_access_headers(client_key, client_secret, base_url)
-    r = requests.get(base_url + 'info/token', headers=headers, verify=True)
-  except:
-    return(
-      # TODO 
-      # {'error': r.NameError}
-      -1
-    )
-
-  return(r.json())
-
-#print('access token expires in: {} seconds'.format(r.json()['expiresIn']))
